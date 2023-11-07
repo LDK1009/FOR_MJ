2c2cfd58901df08a63eaa93de7e937f9edaebc27
LeeDongKyu <m3088787@naver.com>
2023-11-07 17:24:06 +0900

    [FEAT] 새로고침 시 404 오류 해결 시도1

--- for-mj/src/App.js
+++ for-mj/src/App.js
@@ -1,22 +1,22 @@
 import * as React from "react";
-import { Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import SelectFood from "./pages/SelectFood";
 import SelectPlayGround from "./pages/SelectPlayGround";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
-// import AddFoodList from "./components/AddFoodList";
-// import FoodItem from "./components/FoodItem";
-// import LoadingComponent from "./components/LoadingComponent";
+import { BrowserRouter as Router } from "react-router-dom"; // Remove the redundant 'Route' import
 
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/SelectPlayGround" element={<SelectPlayGround />} />
-        <Route path="/SelectFood" element={<SelectFood />} />
-        <Route path="/Diary" element={<Diary />} />
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/SelectPlayGround" element={<SelectPlayGround />} />
+          <Route path="/SelectFood" element={<SelectFood />} />
+          <Route path="/Diary" element={<Diary />} />
+        </Routes>
+      </HashRouter>
     </>
   );
 };
